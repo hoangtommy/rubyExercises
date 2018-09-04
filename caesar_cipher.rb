68a7d37ccb09df8759b1e5dd3c7ee3135978f5a7
Tommy <t@tommyhoang.com>
2018-09-03 21:30:33 -0700

    update comments for clarity

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -7,7 +7,7 @@ PseudoCode:
 	take a string and look each letter's character code on ascii
 	add the number to the string's code, this is the total
 		if the total exceeds 90 (uppercase letters) or 122 (lowercase letters), subtract 26 from it.
-		if it doesn't, merely add the number and get the total
+		if it doesn't, simply add the number and get the total
 	return the encoded string, leaving punctuation and format the same
 =end
 
@@ -20,12 +20,11 @@ end
 
 def caesar_cipher(string, number)
 	newString = ""
-	newChar = 0 #stringCharacterCode + number
 	stringArray = string.split("")
 	i = 0
 	while i < string.length
 		charIndex = stringArray[i].ord
-		newChar = charIndex + number
+		newChar = charIndex + number #the new, shifted, character
 		if charIndex.between?(65,90) #for uppercase
 			if newChar > 90
 				newChar = parse_code(newChar)

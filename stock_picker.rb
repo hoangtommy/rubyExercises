4d3cf7068f761f5c5f48dc9a66d23a793cb60984
Tommy <t@tommyhoang.com>
2018-09-05 11:31:25 -0700

    complete function to account for edge cases

--- stock_picker.rb
+++ stock_picker.rb
@@ -7,3 +7,25 @@
 		#log each date pair and its profit margin. don't change it unless there's a pair date with a larger profit margin
 	#return the date pair
 
+def stock_picker(data) 
+	day1 = 0
+	while day1 < data.length - 1 # 2 < 8
+		day2 = day1 + 1	
+		while day2 < data.length # 3 < 9
+			profit ||= 0
+			if data[day2] - data[day1] > profit #10 - 3 > 12 #7 !> profit
+				profit = data[day2] - data[day1] # 12
+				sellDate = day2 #4
+				buyDate = day1 #1
+			end
+			day2 += 1 #day2 = 9
+		end
+		day1 += 1 #2
+	end
+	return [buyDate, sellDate] 
+end 
+
+data1 = [17,3,6,9,15,8,6,1,10]
+data2 = [1, 10, 2, 2, 4, 5, 15]
+
+p stock_picker(data2)

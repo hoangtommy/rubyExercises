1c3aa0d8707732468fb75c20a6badc189b7bec74
Tommy <t@tommyhoang.com>
2018-09-10 14:10:08 -0700

    add bubble_sort.rb problem

---
+++ bubble_sort.rb
@@ -0,0 +1,24 @@
+#eg [3,2,1] => [2,1,3] => [1,2,3]
+#start with the first element in the list
+	#compare it with the element to its right
+	#if the first element is less than its neighbor, do nothing
+	#if the first element is larger, then swap the two elements' positions
+#do this over and over until the list is sorted from smallest to largest
+
+def bubble_sort(list) #[3,2,1] => [2,3,1] => [2,1,3] => [1,2,3]
+	idx = 0 #1
+	until list[0] == list.min && list[-1] == list.max #2 != 1 && 3 == 3
+		if list[idx] > list[idx + 1] #2 > 1
+			temp = list[idx] #temp = 2
+			list[idx] = list[idx + 1] #list[0] = 1
+			list[idx + 1] = temp #list[1] = 2
+		end
+		idx < list.size - 2 ? idx += 1 : idx = 0 #1 <= 2
+	end
+	p list
+end
+
+puts "type in a list of numeric integers, separated by ','"
+list = gets.chomp
+list = list.split(",").map {|item| item.to_i}
+bubble_sort(list)
--- substrings.rb
+++ substrings.rb
@@ -7,31 +7,22 @@ dictionary = ["below", "below", "down","go","going","horn","how","howdy","it","i
 	# when a match is found, add the match to a hash
 	#
 
-# my attempt
-# def substrings(string, word_list)
-# 	word_count = {}
-# 	word_list.each do |word|
-# 		substring = Regexp.new(/^(be)?(low)?/)
-# 		if word.match?substring #if the word from the list matches (exact or partial) the string...
-# 			if word_count.include?substring 
-# 				word_count[substring] += 1
-# 			else
-# 				word_count[substring] ||= 1
-# 			end
-# 			# word_count.include?string ? word_count[string] += 1 : word_count[string] ||= 1 why won't this work?
-# 		end 
-# 	end
-# 	p word_count
-# end
-
-#another approach: https://github.com/AjaniStewart/ruby-tutorial/blob/master/substrings.rb
 def substrings(string, word_list)
 	word_count = {}
 	word_list.each do |word|
-		total_words = string.scan(word).count 
-		word_count[word] = total_words unless total_words == 0
+		substring = /^(be)?(low)?/
+		if word.match?substring #if the word from the list matches (exact or partial) the string...
+			if word_count.include?substring 
+				word_count[substring] += 1
+			else
+				word_count[substring] ||= 1
+			end
+			# word_count.include?string ? word_count[string] += 1 : word_count[string] ||= 1 why won't this work?
+		end 
 	end
 	p word_count
 end
+
+
 
 substrings("below", dictionary)

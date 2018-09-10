2773431c0015dd35b74dbe1682e1ff1dcb2d3486
Tommy <t@tommyhoang.com>
2018-09-10 15:13:22 -0700

    complete method for accepting block

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -22,7 +22,33 @@ def bubble_sort(list)
 	p list
 end
 
+def bubble_sort_by(list) #["hi", "hello", "hey"]
+	#start by passing the list's first and second numbers to the block
+		#the block will compare the two numbers by substracting the second from the first
+		#the block will return an integer to the method
+		#if the integer is positive: the first number is larger than the second. we need to switch their order
+		#if the integer is negative or 0, we don't do anything
+	#we'll continue to pass pairs of numbers to the block, and switching them as needed according to the result
+		#we'll do this process list.size - 1 number of times
+	last = list.size - 1
+	list.size.times do #2.times
+		idx = 0
+		while idx < last #1 < 2
+			result = yield(list[idx], list[idx + 1]) #yield("hey", "hello")
+			if result > 0 # > 0
+				temp = list[idx] #temp = 2
+				list[idx] = list[idx + 1] #list[idx] = "hey"
+				list[idx + 1] = temp #"hello"
+			end
+			idx += 1
+		end
+		last -= 1
+	end
+	p list
+end
+
+
 puts "type in a list of numeric integers, separated by ','"
 list = gets.chomp
-list = list.split(",").map {|item| item.to_i}
-bubble_sort(list)
+list = list.split(",")
+bubble_sort_by(list) {|first, second| first.length - second.length} #2 - 3
